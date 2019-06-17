fdcbecafa6be73fe303903b0a1713ea15ef43ffc
Jette Bakemeier <jette.bakemeier@gmail.com>
2019-06-17 14:12:45 +0100

    added mailing feature in development and styled review on home page

---
+++ app/mailers/booking_mailer.rb
@@ -0,0 +1,13 @@
+class BookingMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.booking_mailer.confirmation.subject
+  #
+  def confirmation
+    @booking = Booking.last
+
+    mail(to: "annkathrin.thesen@web.de", subject: "Someone is interested in working with you!")
+  end
+end
