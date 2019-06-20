c8e8b889cc93b5d55c469c6ef9e253821d9e6b0a
Jette Bakemeier <jette.bakemeier@gmail.com>
2019-06-20 23:42:57 +0100

    moved controller action to pages controller, still doesnt work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,25 @@
 class PagesController < ApplicationController
   def home
     @booking = Booking.new
-    @bookings = Booking.all # and ultimately I suppose: Booking.where(user: current_user)
+    @bookings = Booking.all
+  end
+
+  def create_booking
+    @booking = Booking.create(booking_params)
+    if @booking.save
+      session[:booking_id] = @booking.id
+      flash[:success] = "New booking created."
+      mail = BookingMailer.with(booking: @booking).confirmation
+      mail.deliver_now
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:name, :email, :options, :message)
   end
 end
