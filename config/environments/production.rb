464496d010f7274389d7571948a2eb6e14c93442
Jette Bakemeier <jette.bakemeier@gmail.com>
2019-06-21 19:50:14 +0100

    trying to make the mailer work on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,9 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: "anni-thesen.herokuapp.com" }
+  config.action_mailer.default_options = { from: 'anni.performatory@gmail.com'}
   # or your custom domain name eg. "www.yourdomain.com"
 
   # Ignore bad email addresses and do not raise email delivery errors.
